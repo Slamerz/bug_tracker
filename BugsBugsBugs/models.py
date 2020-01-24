84d177f517034c896a5942ce47601a86a15ea310
Unknown <keybladeschoosen1@gmail.com>
2020-01-24 14:57:08 -0500

    initial models and migrations

--- BugsBugsBugs/models.py
+++ BugsBugsBugs/models.py
@@ -1,4 +1,24 @@
 from django.db import models
 
+
 class Ticket(models.Model):
-    title = models.CharField()
+    NEW = 'NEW'
+    IN_PROGRESS = 'INPROGRESS'
+    DONE = 'DONE'
+    INVALID = 'INVALID'
+    CHOICES = [
+        (NEW, 'New'),
+        (IN_PROGRESS, 'In Progress'),
+        (DONE, 'Done'),
+        (INVALID, 'Invalid')
+    ]
+    title = models.CharField(max_length=180)
+    date_Filed = models.DateTimeField('dateFiled', auto_now=True)
+    description = models.CharField(max_length=300)
+    filed_By = models.CharField(max_length=50)
+    assigned_To = models.CharField(max_length=50)
+    completed_by = models.CharField(max_length=50)
+    status = models.CharField(choices=CHOICES, max_length=11, default=NEW)
+
+    def __str__(self):
+        return f'{self.title}'
